8f729663d3f97bef4a1ca4ed98fa61e4d7214091
Shehbaz <mdshehbaz666666@gmail.com>
2025-04-30 10:55:53 +0530

    problem controller and routes for CRUD operations

---
+++ backend/src/controllers/problem.controller.js
@@ -0,0 +1,16 @@
+import { db } from "../libs/db";
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+
+export const createProblem = asyncHandler((req, res) => {});
+
+export const getAllProblems = asyncHandler((req, res) => {});
+
+export const getProblemById = asyncHandler((req, res) => {});
+
+export const updateProblem = asyncHandler((req, res) => {});
+
+export const deleteProblem = asyncHandler((req, res) => {});
+
+export const getAllProblemsSolvedByUser = asyncHandler((req, res) => {});
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,23 +3,23 @@ dotenv.config();
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
-const app = express()
-const PORT = process.env.PORT || 8000
+import problemRoutes from "./routes/problem.routes.js";
+const app = express();
+const PORT = process.env.PORT || 8000;
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // home route
-app.get("/" , (req , res)=>{
+app.get("/", (req, res) => {
     res.send("Hello Guys welcome to Code Execution Lab🔥");
-})
+});
 
-app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/problems", problemRoutes);
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is Running on port ${PORT}`);  
-})
-
-
+app.listen(process.env.PORT, () => {
+    console.log(`Server is Running on port ${PORT}`);
+});
